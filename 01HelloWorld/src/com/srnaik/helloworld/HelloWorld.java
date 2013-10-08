c2d4e9829d14bde9e4aa9249241aa454ae6fe187
sachi <srnaik@hotmail.com>
2013-10-08 07:52:37 +0530

    First Java program completed...

--- 01HelloWorld/src/com/srnaik/helloworld/HelloWorld.java
+++ 01HelloWorld/src/com/srnaik/helloworld/HelloWorld.java
@@ -1,7 +1,22 @@
 package com.srnaik.helloworld;
 
+/**
+ * 
+ * @author srnaik
+ * 
+ * This class is a simple Java class.
+ *
+ */
+
 public class HelloWorld {
 
+	/**
+	 *@param args
+	 *
+	 *This is the main method in Java, main methods 
+	 *are always declared as static so that they 
+	 *can be invoked with out creating any object of the class.
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("Hello World !!!");
